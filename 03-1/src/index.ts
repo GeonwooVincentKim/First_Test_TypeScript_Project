8ef3b5830a56f41e6fae0a392eb7235063565002
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-08 21:25:15 +0900

    [Feature] - Index.ts has been upgraded

--- 03-1/src/index.ts
+++ 03-1/src/index.ts
@@ -4,3 +4,12 @@ let s: string = 'hello'
 let o: object = {}
 console.log("n -> ", n + "\nb -> " + b, "\ns -> " + s, "\no -> " + o)
 console.log("\n--------------------------------\n")
+
+let a: any = 0
+a = 'hello'
+console.log("a -> ", a)
+a = true
+console.log("a -> ", a)
+a = {}
+console.log("a -> ", a)
+console.log("\n--------------------------------\n")
