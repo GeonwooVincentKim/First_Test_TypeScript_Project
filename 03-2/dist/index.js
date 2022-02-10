0e25c62520919bae6129d23c33d247701297ad21
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-10 21:48:28 +0900

    [Update] - Build TypeScript with `NPM`

--- 03-2/dist/index.js
+++ 03-2/dist/index.js
@@ -1,1 +1,13 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const objectAndInterface_1 = require("./utils/objectAndInterface");
+let good = { name: 'Jack', age: 32 };
+console.log("IPerson -> ", good);
+console.log("\n--------------------------------------\n");
+let good2 = { name: 'Jack', age: 32 };
+console.log("IPerson1-1 -> ", good2);
+let good3 = { name: 'Jack', age: 32, etc: true };
+console.log("IPerson1-2 -> ", good3);
+console.log("\n---------------------------------------\n");
+console.log("IPerson Anonymous-Function", objectAndInterface_1.ai);
+console.log("\n---------------------------------------\n");
