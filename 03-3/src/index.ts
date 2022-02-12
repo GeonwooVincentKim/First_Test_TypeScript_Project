653324224538b2a3ef094679904cc09d0be573a6
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-12 23:01:14 +0900

    [Feature] - Created Workspace and Modified 03-2 TSConfig.jSON

---
+++ 03-3/src/index.ts
@@ -0,0 +1,9 @@
+import { Person1 } from "./utils/classAndObject";
+
+let jack1: Person1 = new Person1()
+jack1.name = 'Jack'; jack1.age = 32;
+
+console.log("Jack -> ", jack1)
+console.log("Jack Name -> ", jack1.name);
+console.log("Jack Age -> ", jack1.age);
+
