1ab9df57ce260e17bce522acfa22e23b5ea37ad7
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-12 23:54:11 +0900

    [Feature] - Testing Public-status Person Class

--- 03-3/src/index.ts
+++ 03-3/src/index.ts
@@ -1,4 +1,4 @@
-import { Person1, Person2 } from "./utils/classAndObject";
+import { Person1, Person2, Person3 } from "./utils/classAndObject";
 
 let jack1: Person1 = new Person1()
 jack1.name = 'Jack'; jack1.age = 32;
@@ -11,3 +11,8 @@ console.log("\n--------------------------\n")
 
 let jack2: Person2 = new Person2('Jack', 32)
 console.log(jack2)
+
+console.log("\n--------------------------\n")
+
+let jack3: Person3 = new Person3('Jack', 32);
+console.log(jack3);
--- 03-3/src/utils/classAndObject.ts
+++ 03-3/src/utils/classAndObject.ts
@@ -6,3 +6,12 @@ export class Person1 {
 export class Person2 {
     constructor(public name: string, public age?: number) {}
 }
+
+export class Person3 {
+    name: string
+    age?: number
+    
+    constructor(name: string, age?: number){
+        this.name = name; this.age = age
+    }
+}
