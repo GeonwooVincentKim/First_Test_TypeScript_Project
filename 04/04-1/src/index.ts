4b297fad70a956cb7dd33b06222932d0af2f3a8a
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-13 20:19:00 +0900

    [Update] - Updated TypeAlias.ts file

--- 04/04-1/src/index.ts
+++ 04/04-1/src/index.ts
@@ -1,5 +1,9 @@
 import { printMe } from "./utils/comments";
+import { f, g } from "./utils/type-alias";
 
-console.log(printMe('Rachel', 32));
+console.log("printMe -> ", printMe('Rachel', 32));
 console.log("\n---------------------------------\n");
 
+console.log("F -> ", f);
+console.log("G -> ", g);
+console.log("\n---------------------------------\n");
--- 04/04-1/src/utils/type-alias.ts
+++ 04/04-1/src/utils/type-alias.ts
@@ -1,3 +1,4 @@
 type stringNumberFunc = (string, number) => void
-let f: stringNumberFunc = function(a: string, b: number): void {}
-let g: stringNumberFunc = function(c: string, d: number): void {}
+export let f: stringNumberFunc = function(a: string, b: number): void {}
+export let g: stringNumberFunc = function(c: string, d: number): void {}
+console.log(f, g);
