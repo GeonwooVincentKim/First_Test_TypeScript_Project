a579e8f43f62cc4a351a65ac0022316ae7e94174
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-21 21:34:10 +0900

    [Feature] - Print the result

--- 04/04-2/src/index.ts
+++ 04/04-2/src/index.ts
@@ -1,5 +1,16 @@
 import { result } from "./utils/Function";
 import { add2 } from "./utils/function-expression";
+import { f } from "./utils/first-class";
+import { functionExpression, value } from "./utils/function-call";
 
 console.log("Result (After Import) -> ", result);
 console.log("Add2 (After Import) -> ", add2(1, 2));
+console.log("\n------------------------\n")
+
+console.log("First Class -> ", f(1, 2));
+console.log("\n------------------------\n")
+
+console.log("Function Expression -> ", functionExpression(1, 2));
+console.log("Value -> ", value);
+console.log("\n------------------------\n")
+
---
+++ 04/04-2/src/utils/function-call.ts
@@ -0,0 +1,2 @@
+export let functionExpression = function(a, b) {return a + b}
+export let value = functionExpression(1, 2)
