f5483cf416f4b6827c41af6ba92f48f3721f4696
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-21 21:37:28 +0900

    [Update] - Update Anonymous.ts file

--- 04/04-2/src/index.ts
+++ 04/04-2/src/index.ts
@@ -2,6 +2,7 @@ import { result } from "./utils/Function";
 import { add2 } from "./utils/function-expression";
 import { f } from "./utils/first-class";
 import { functionExpression, value } from "./utils/function-call";
+import { value2, f1, f2 } from "./utils/anonymous";
 
 console.log("Result (After Import) -> ", result);
 console.log("Add2 (After Import) -> ", add2(1, 2));
@@ -14,3 +15,6 @@ console.log("Function Expression -> ", functionExpression(1, 2));
 console.log("Value -> ", value);
 console.log("\n------------------------\n")
 
+console.log("Value 2 -> ", value2);
+console.log("Let -> ", f1);
+console.log("Const -> ", f2);
---
+++ 04/04-2/src/utils/anonymous.ts
@@ -0,0 +1,3 @@
+export let value2 = (function(a, b) {return a + b;})(1, 2)
+export let f1 = () => {}
+export const f2 = () => {}
