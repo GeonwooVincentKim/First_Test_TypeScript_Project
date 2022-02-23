664e6c7441b6e0139a4ca74e200ee1d7b646d736
GeonwooVincentKim <kdsnop@gmail.com>
2022-02-23 22:28:33 +0900

    [Update] - Print Function-Expression result

---
+++ 04/04-3/src/index.ts
@@ -0,0 +1,4 @@
+import { arrow1, arrow2 } from "./utils/expression";
+
+console.log("Arrow1 -> ", arrow1);
+console.log("Arrow2 -> ", arrow2);
---
+++ 04/04-3/src/utils/expression.ts
@@ -0,0 +1,2 @@
+export const arrow1 = (a: number, b: number): number => {return a + b}
+export const arrow2 = (a: number, b: number): number => a + b
