fbff76a3b78e84034fe4fb354ae8c82b6aa83633
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-06 21:32:38 +0900

    [Feature] - Index.ts and Expression.ts file has been upgraded

--- 04/04-3/src/index.ts
+++ 04/04-3/src/index.ts
@@ -1,4 +1,9 @@
-import { arrow1, arrow2 } from "./utils/expression";
+import { arrow1, arrow2, f, isGreater } from "./utils/expression";
 
 console.log("Arrow1 -> ", arrow1);
 console.log("Arrow2 -> ", arrow2);
+console.log("\n----------------------------\n");
+
+console.log();
+console.log("`F` Function -> ", f);
+console.log("isGreater -> ", isGreater(10, 20));
--- 04/04-3/src/utils/expression.ts
+++ 04/04-3/src/utils/expression.ts
@@ -1,2 +1,11 @@
 export const arrow1 = (a: number, b: number): number => {return a + b}
 export const arrow2 = (a: number, b: number): number => a + b
+
+export function f(){
+    let x = 1, y = 2
+    let result = x + y + 10
+}
+
+export function isGreater(a: number, b: number) : boolean { 
+    return a > b
+}
