a7d80ddd8cd9c7ba7a267520ef6ae95cf014abf8
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-06 21:57:44 +0900

    [Update] - Updated Index.ts file

--- 04/04-3/src/index.ts
+++ 04/04-3/src/index.ts
@@ -4,7 +4,11 @@ console.log("Arrow1 -> ", arrow1);
 console.log("Arrow2 -> ", arrow2);
 console.log("\n----------------------------\n");
 
-console.log();
 console.log("`F` Function -> ", f);
 console.log("isGreater -> ", isGreater(10, 20));
 console.log("isGreater (Const & Arrow Function) -> ", isGreater2(10, 20));
+console.log("\n----------------------------\n");
+
+let a = 1, b = 0
+if (a > b) console.log('A is greater than B')
+console.log("Check A is greater than B", isGreater2(a, b));
