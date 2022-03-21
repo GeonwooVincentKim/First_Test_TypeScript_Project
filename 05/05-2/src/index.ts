8e73b3dc55b77dd92f2664f018efcaec47cf4dfd
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-21 22:07:04 +0900

    [Feature] - Displayed the result of Pure Sort

--- 05/05-2/src/index.ts
+++ 05/05-2/src/index.ts
@@ -1,11 +1,13 @@
 import { evenResult } from "./utils/declarative-even-sum";
 import { squareSum } from "./utils/imperative-square-sum";
+import { afterSort, beforeSort } from "./utils/pureSort-test";
 
-console.log("\nDeclarative-Even-Sum Result -> ", evenResult);
+console.log("\n------------------------------------");
+console.log("Declarative-Even-Sum Result -> ", evenResult);
 console.log("--------------------------------------");
 
 console.log("Square Sum -> ", squareSum);
 console.log("--------------------------------------");
 
-console.log("Square Sum -> ", squareSum);
+console.log("Pure Sort (BeforeSort, AfterSort) -> ", beforeSort, afterSort);
 console.log("--------------------------------------\n");
