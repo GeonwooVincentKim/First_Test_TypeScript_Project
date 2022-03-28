4cbc7b8f5a5fb852a4b62fa6ddd45ebe5ba9271e
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-28 22:47:54 +0900

    [Feature] - Iterable and Iterator

--- 06/06-1/src/index.ts
+++ 06/06-1/src/index.ts
@@ -1,11 +1,12 @@
 import { createRangeIterable } from "./utils/createRangeIterable";
 import { iterator } from "./utils/RangeIterable-test";
+import { StringIterable } from "./utils/StringIterable";
 
 console.log("\n========================================");
 console.log("Value -> \n")
 for (let value of iterator) console.log(value)
 
-
-console.log("\n----------------------------------------");
+console.log("------------------------------------------");
+for (let value of new StringIterable(['Hello', 'world', '!'])) console.log(value);
 
 console.log("========================================\n");
