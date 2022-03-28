8b91eb95a48ee1091bfc07bdf80382cd65fb241f
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-28 18:38:34 +0900

    [Update] - Updated CreatedIterable-ForOf.ts

--- 06/06-1/src/index.ts
+++ 06/06-1/src/index.ts
@@ -1,10 +1,7 @@
 import { createRangeIterable } from "./utils/createRangeIterable";
 
 console.log("\n========================================");
-const iterable = createRangeIterable(1, 3 + 1)
 
-for (let value of iterable) 
-    console.log(value);
 
 console.log("\n----------------------------------------");
 
--- 06/06-1/src/utils/createIterable-ForOf.ts
+++ 06/06-1/src/utils/createIterable-ForOf.ts
@@ -1,3 +1,9 @@
 import { createRangeIterable } from "./createRangeIterable";
 
 export const iterable = createRangeIterable(1, 3 + 1)
+
+/*
+   Triggers `[System.iterator]` Method needs
+*/
+// for (let value of iterable) 
+//     console.log(value);
